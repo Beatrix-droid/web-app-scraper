921f49f5ef74300723d5095879a9da46435f9a92
Beatrice <amilcare8@gmail.com>
2021-11-14 21:59:38 +0000

    backend  now captures data users  enter in form

--- main.py
+++ main.py
@@ -7,15 +7,22 @@ app = Flask(__name__)
 
 
 @app.route("/", methods=["GET", "POST"])
-def get_dev_jobs():
-	if request.method =="POST":
-		req = request.form
-		return redirect(request.url)
+def post_form():
+	if request.method == "POST":
+		link = request.form["link"]
+		tag = request.form["tag"]
+		return redirect("/result")
 	return render_template("index.html")
+
+
+@app.route("/result", methods=["POST"])
+def show_results():
+	return render_template("result.html")
 
 
 if __name__ == "__main_":
 	app.run(debug=True, port=8000)
+
 
 
 	#html_text = requests.get("https://www.reed.co.uk/jobs/python-jobs").text
