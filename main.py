0d2e0492388f63e1e87f3cde4fa08ab013241bbc
Beatrice <amilcare8@gmail.com>
2021-11-15 15:10:03 +0000

    bug fix
    fixed bug that was causing the captured data in the form to not render properly in the results page

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
 import requests
-#import lxml
+
+from urllib.parse import urljoin, urlparse
+
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def post_form():
-	if request.method == "POST":
-		global link, tag, desired_element
-		link = request.form["link"]
-		tag = request.form["tag"]
-		html_text = requests.get(f"{link}").text
-		soup = BeautifulSoup(html_text, "lxml")
-		desired_element = soup.find_all(f"{tag}")
-		counter = len(desired_element)
-		return redirect("/result", url=link, results=desired_element, counter=counter)
 	return render_template("index.html")
 
 @app.route("/result", methods=["POST"])
 def show_results():
-	return render_template("result.html")
-
+	global link, tag, desired_element
+	link = request.form.get("link")
+	tag = request.form.get("tag")
+	html_text = requests.get(link).text
+	soup = BeautifulSoup(html_text, "html.parser")
+	desired_element = soup.find_all(tag)
+	counter = len(desired_element)
+	return render_template("result.html", name="TIM", url=link, results=desired_element)
 
 if __name__ == "__main_":
 	app.run(debug=True, port=8000)
