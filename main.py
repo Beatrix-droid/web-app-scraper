8b359888a0bbe53242de6c9e68ff91a5e115548b
Beatrice <amilcare8@gmail.com>
2021-11-16 15:54:17 +0000

    added try except
    rather than crashing the app, users are now redirected to a brand new  errorl.html page if they enter data in the form incorrectly

--- main.py
+++ main.py
@@ -16,12 +16,15 @@ def post_form():
 def show_results():
 	global link, tag, desired_element
 	link = request.form.get("link")
-	tag = request.form.get("tag")
-	html_text = requests.get(link).text
-	soup = BeautifulSoup(html_text, "html.parser")
-	desired_element = soup.find_all(tag)
-	counter = len(desired_element)
-	return render_template("result.html", name="TIM", url=link, results=desired_element)
+	try:
+		tag = request.form.get("tag")
+		html_text = requests.get(link).text
+		soup = BeautifulSoup(html_text, "html.parser")
+		desired_element = soup.find_all(tag)
+		counter = len(desired_element)
+		return render_template("result.html", tag=tag, url=link, count=counter, results=desired_element)
+	except:
+		return render_template("error.html")
 
 if __name__ == "__main_":
 	app.run(debug=True, port=8000)
