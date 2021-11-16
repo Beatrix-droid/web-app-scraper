7cc4e0778f27f7cef247acf984557ab2b112c4b9
Beatrice <amilcare8@gmail.com>
2021-11-16 16:39:38 +0000

    added initial image scraping functionality

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from flask import Flask, render_template, request
 from bs4 import BeautifulSoup
 import requests
+from image_scraper import *
 
 from urllib.parse import urljoin, urlparse
 
 
 app = Flask(__name__)
-
-
-@app.route("/")
-def post_form():
-	return render_template("index.html")
-
 @app.route("/result", methods=["POST"])
 def show_results():
 	global link, tag, desired_element
@@ -22,7 +17,9 @@ def show_results():
 		soup = BeautifulSoup(html_text, "html.parser")
 		desired_element = soup.find_all(tag)
 		counter = len(desired_element)
-		return render_template("result.html", tag=tag, url=link, count=counter, results=desired_element)
+		image_paths = image_handler(tag, desired_element, link)
+		return render_template("result.html", tag=tag, url=link, images=image_paths, count=counter, 
+				       results=desired_element)
 	except:
 		return render_template("error.html")
 
