909d69384ea6704bff3a4cd0f906e41300b7de13
Beatrice <amilcare8@gmail.com>
2021-11-17 15:19:02 +0000

    -readded home page route

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ from bs4 import BeautifulSoup
 import requests
 from image_scraper import *
 
-from urllib.parse import urljoin, urlparse
 
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def post_form():
+	return render_template("index.html")
+
 @app.route("/result", methods=["POST"])
 def show_results():
 	global link, tag, desired_element
@@ -18,10 +23,15 @@ def show_results():
 		desired_element = soup.find_all(tag)
 		counter = len(desired_element)
 		image_paths = image_handler(tag, desired_element, link)
-		return render_template("result.html", tag=tag, url=link, images=image_paths, count=counter, 
-				       results=desired_element)
+		return render_template("result.html", tag=tag, url=link, images=image_paths, count=counter, results=desired_element)
 	except:
 		return render_template("error.html")
 
 if __name__ == "__main_":
-	app.run(debug=True, port=8000)
+	app.run(debug=True)
+
+
+
+
+
+
