d965995f9fbc88f1ba311b8fec37b60755fd8587
Beatrice <amilcare8@gmail.com>
2021-11-18 22:18:26 +0000

    tweaked code to follow PEP * guidelines

--- image_scraper.py
+++ image_scraper.py
@@ -1,24 +1,22 @@
 import validators
 from urllib.parse import urljoin
 
+
 def image_handler(tag, desired_element, link):
-		""" Function check if the requeste tag is an image. If it is, it looks at
-		its path (via 'src'). The validator checks if the image path is a URL.
-		If it is the image path is appended to the image paths_list. If it is not
-		the case, urljoin joins the image relative path to the base URL of the website"""
+	""" Function check if the requeste tag is an image. If it is, it looks at
+	its path (via 'src'). The validator checks if the image path is a URL.
+	If it is the image path is appended to the image paths_list. If it is not
+	the case, urljoin joins the images relative path to the base URL of the website"""
 
-		image_paths = []
-		if tag =="img":
-			images = [img["src"] for img in desired_element]
-			for i in desired_element:
-				image_path = i.attrs["src"]
-				validated_image_path = validators.url(image_path)
-				if validated_image_path == True:
-					full_path = image_path
-				else:
-					full_path = urljoin(link, image_path)
-					image_paths.append(full_path)
-		return image_paths
-
-
-
+	image_paths = []
+	if tag == "img":
+		images = [img["src"] for img in desired_element]
+		for i in desired_element:
+			image_path = i.attrs["src"]
+			validated_image_path = validators.url(image_path)
+			if validated_image_path:
+				full_path = image_path
+			else:
+				full_path = urljoin(link, image_path)
+				image_paths.append(full_path)
+	return image_paths
